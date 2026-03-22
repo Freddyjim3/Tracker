db57e63418261e4da1178952055b62823ae18027
Freddyjim3 <lifetravel.mm19@gmail.com>
2026-03-22 23:56:54 +0630

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.supabaseConfig = {
   url: "https://ixfshuitkdpykaqwmfrb.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZnNodWl0a2RweWthcXdtZnJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxODg5NzAsImV4cCI6MjA4OTc2NDk3MH0.CkUVhuNTAFJSan2XQPoKydgCQmGmyYsijJjkrjw1QeM"
+  anonKey: "sb_publishable_9T-wjQgZG6I67zuZLeU50w_pX9FtUPB"
 };
